a9d7d2ed8a0585072d9a131732df0f8201dc021c
Felipe Silveira <eng.fe.silveira@gmail.com>
2022-10-07 05:53:46 +0200

    feat: add faker and test coverage report

--- src/modules/createOrder/CreateOrderController.spec.ts
+++ src/modules/createOrder/CreateOrderController.spec.ts
@@ -1,19 +1,26 @@
 import { app } from "../../app";
 import request from "supertest";
+import { faker } from "@faker-js/faker";
 
 describe("Create Order Controller", () => {
   it("should be able to create a new order", async () => {
-    const response = await request(app).post("/v1/orders").send({
-      isPrimary: "Y",
-      title: "test title",
-      firstName: "first_name",
-      lastName: "last_name",
-      dateOfBirth: "dob",
-      nationalId: "ssn",
-      percentageOwnership: 100,
-      taxId: "ssn",
-      nationalIdType: "SSN",
-    });
+    const response = await request(app)
+      .post("/v1/orders")
+      .send({
+        isPrimary: faker.helpers.arrayElement(["Y", "N"]),
+        title: faker.lorem.word(5),
+        firstName: faker.name.firstName(),
+        lastName: faker.name.lastName(),
+        dateOfBirth: faker.date.birthdate(),
+        nationalId: "ssn",
+        percentageOwnership: faker.datatype.number({
+          min: 10,
+          max: 100,
+          precision: 1,
+        }),
+        taxId: "ssn",
+        nationalIdType: "SSN",
+      });
 
     expect(response.status).toBe(200);
     expect(response.body).toHaveProperty("id");
